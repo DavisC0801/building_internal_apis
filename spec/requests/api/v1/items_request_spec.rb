71e951eb5e8d4418ff8bff35ad8f9b8bf742d6ed
davisc0801 <davisc1014@gmail.com>
2019-06-24 14:37:21 -0600

    Add test for individual item API call

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -4,12 +4,24 @@ describe "Items API" do
   it "sends a list of items" do
     create_list(:item, 3)
 
-    get '/api/v1/items'
+    get "/api/v1/items"
 
     expect(response).to be_successful
 
     items = JSON.parse(response.body)
 
     expect(items.count).to eq(3)
+  end
+
+  it "can get a single item by ID" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body)
+
+    expect(item["id"]).to eq(id)
   end
 end
