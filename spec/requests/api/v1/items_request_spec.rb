0f34be6744c6b495e2fde5f4a42fc57ce068b0b2
davisc0801 <davisc1014@gmail.com>
2019-06-24 14:52:19 -0600

    Add update item test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -33,6 +33,20 @@ describe "Items API" do
 
     expect(response).to be_successful
     expect(item.name).to eq(item_params[:name])
-    expect(response.description).to eq(item_params[:description])
+    expect(item.description).to eq(item_params[:description])
+  end
+
+  it "can update an item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+
+    item_params = {name: "Sledge"}
+
+    put "/api/v1/items/#{id}", params: {item: item_params}
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Sledge")
   end
 end
