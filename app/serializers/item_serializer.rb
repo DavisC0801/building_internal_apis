d427f260404302968453ebece8c430dcce51f4ab
davisc0801 <davisc1014@gmail.com>
2019-06-25 13:33:53 -0600

    Add relationship to item serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,9 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
+  has_many :orders
   attributes :id, :name, :description
+
+  attribute :num_orders do |object|
+    object.items.count
+  end
 end
